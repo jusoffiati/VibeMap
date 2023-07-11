34b369eb18a0b1b86a12339da4e6b3742c7b1530
jusoffiati <jusoffiati@github.com>
2023-07-11 20:51:42 +0200

    updated models.index.js to use .env variables

--- server/models/models.index.js
+++ server/models/models.index.js
@@ -1,9 +1,15 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize("VibeMap", "postgres", "admin", {
-    host: "localhost",
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
     dialect: "postgres",
-    port: 5433,
-});
+    port: 5432,
+  }
+);
 
-module.exports = sequelize
+module.exports = sequelize;
